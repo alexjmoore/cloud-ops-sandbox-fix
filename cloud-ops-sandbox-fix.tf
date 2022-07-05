6d57a7e960856223ffed1581f05cd41bcce5278a
Alex Moore <alex@moore.mx>
2022-07-05 11:45:02 +0100

    initial checkin

---
+++ cloud-ops-sandbox-fix.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+variable "cloud-ops-project" {
+  type = string
+}
+
+provider "google" {
+    project = var.cloud-ops-project
+}
+
+resource "google_compute_network" "default" {
+  name                    = "default"
+  auto_create_subnetworks = true
+}
+
+resource "google_project_organization_policy" "oslogin_policy" {
+  project    = var.cloud-ops-project 
+  constraint = "compute.requireOsLogin"
+
+  boolean_policy {
+    enforced = false
+  }
+}
+
+resource "google_project_organization_policy" "shieldedvm_policy" {
+  project    = var.cloud-ops-project 
+  constraint = "compute.requireShieldedVm"
+
+  boolean_policy {
+    enforced = false
+  }
+}
+
+resource "google_project_organization_policy" "externalip_policy" {
+  project    = var.cloud-ops-project 
+  constraint = "compute.vmExternalIpAccess"
+
+  list_policy {
+    allow {
+      all = true
+    }
+  }
+}
